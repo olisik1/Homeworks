5a88ca83bd6b0c99475cc0a07df4abb38ada1ef5
olis_sosisolis <alise.study@gmail.com>
2024-06-08 22:33:03 +0300

    Delete homework5.py

--- homework5.py
+++
@@ -1,9 +0,0 @@
-immutable_var=tuple([15,'forest',True])
-print(immutable_var)
-#immutable_var[15]=25
-#print(immutable_var) я специально вывела в коммент, иначе код не срабатывает
-#Кортежи являются неизменяемыми последовательностями. Это означает, что после того как кортеж создан, его невозможно изменить.
-mutable_list=['blue', 'white', 'yellow', 'orange']
-print(mutable_list)
-mutable_list[0]='green'
-print(mutable_list)
