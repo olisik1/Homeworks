201e6ec16c58825066ccf7cdc51d69acb292565d
olis_sosisolis <alise.study@gmail.com>
2024-06-09 16:49:05 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,39 @@
+the_lucky_number={'Vova':2201, 'Alice':21, 'Dima':33}
+print(the_lucky_number)
+print(the_lucky_number['Vova'])
+print(the_lucky_number.get('Boris'))
+the_lucky_number.update({'Lena':37,
+                        'Danil':42})
+a=the_lucky_number.pop('Dima')
+print(a)
+print(the_lucky_number)
+
+my_set={1,3,3,5,5,5,True,'pink', (2,3,4)}
+print(my_set)
+my_set.add(21)
+my_set.add('yellow')
+my_set.remove(3)
+print(my_set)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
