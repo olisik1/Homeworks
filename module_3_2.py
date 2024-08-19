56217d574fe942623b38da46851147537fe35c6c
olis_sosisolis <alise.study@gmail.com>
2024-08-19 14:57:01 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,26 @@
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    # Проверка корректности email для отправителя и получателя
+    valid_domains = [".com", ".ru", ".net"]
+    def is_valid_email(email):
+        return "@" in email and any(email.endswith(domain) for domain in valid_domains)
+
+    if not is_valid_email(recipient) or not is_valid_email(sender):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        return
+
+    # Проверка на отправку самому себе
+    if sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+        return
+
+    # Проверка на отправителя по умолчанию
+    if sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
+
+# Примеры выполнения функции:
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
