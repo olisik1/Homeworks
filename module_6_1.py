576dae8c36a03769c4d1b84337ac5e435b33a6cd
olis_sosisolis <alise.study@gmail.com>
2024-08-19 15:59:29 +0300

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,56 @@
+# Описание родительских классов и их атрибутов
+class Animal:
+    def __init__(self, name):
+        self.alive = True
+        self.fed = False
+        self.name = name
+
+class Plant:
+    def __init__(self, name, edible=False):
+        self.edible = edible
+        self.name = name
+
+# Описание подклассов для Animal: Mammal и Predator
+class Mammal(Animal):
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
+class Predator(Animal):
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
+# Описание подклассов для Plant: Flower и Fruit
+class Flower(Plant):
+    def __init__(self, name):
+        super().__init__(name, edible=False)
+
+class Fruit(Plant):
+    def __init__(self, name):
+        super().__init__(name, edible=True)
+
+# Создание объектов классов для демонстрации работы программы
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+# Демонстрация работы методов и атрибутов
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
