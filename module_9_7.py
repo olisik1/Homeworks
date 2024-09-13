d61bc1c33003639e3dd34570316882e4a9cd4327
olis_sosisolis <alise.study@gmail.com>
2024-09-13 11:50:42 +0300

    Update module_9_7.py

--- module_9_7.py
+++ module_9_7.py
@@ -2,8 +2,9 @@ def is_prime(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
 
-        # Проверяем, простое ли число
-        if result < 2:
+        if result == 1:
+            print("Число 1 не является ни простым, ни составным")
+        elif result < 1:
             print("Составное")
         else:
             for i in range(2, int(result ** 0.5) + 1):
@@ -23,6 +24,9 @@ def sum_three(a, b, c):
 
 # Пример использования
 result = sum_three(2, 3, 6)
+print(result)
+
+result = sum_three(1, 0, 0)  # Проверка для числа 1
 print(result)
 
 result = sum_three(2, 2, 2)
